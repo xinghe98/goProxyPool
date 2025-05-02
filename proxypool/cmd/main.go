c9fb1fc38c1f6d83258d9a068087366d93010739
xinghe98 <lcg9899@gmail.com>
2025-05-02 09:34:24 +0800

    add viper config

--- proxypool/cmd/main.go
+++ proxypool/cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"github.com/xinghe98/goProxyPool/common"
+)
 
 func main() {
-	fmt.Println("hell word")
+	common.InitConfig()
+	url := viper.GetString("DB.url")
+	fmt.Println(url)
 }
